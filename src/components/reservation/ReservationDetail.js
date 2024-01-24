e2e369671aaf450a2eb6b404505d4a35884fda2c
jkeum-dev <jon9eun@gmail.com>
2024-01-24 18:13:48 +0900

    fix : 충돌 해결

--- src/components/reservation/ReservationDetail.js
+++ src/components/reservation/ReservationDetail.js
@@ -80,9 +80,6 @@ export default function ReservationDetail({ id }) {
 						/>
 					</div>
 					<div style={styles.paymentInfo}>
-						<div style={styles.paymentMethodBox}></div>
-					</div>
-					<div style={styles.paymentInfo}>
 						<div style={styles.detailsRow}>
 							<span>결제 금액</span>
 							<span>{`${reservationData.paidPrice}원`}</span>
