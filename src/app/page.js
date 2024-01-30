46372891d8165ed78e5b0e5b83f2ad1e592949a8
kyumho kim <96820952+js030@users.noreply.github.com>
2024-01-30 13:20:15 +0900

    Navbar 디자인 수정, 찜 기능 추가(로그인 된 유저만 가능) (#5)
    * design : Navbar 디자인 수정
    
    * design : navbar 디자인 수정
    
    * feat : 숙소 찜 기능 추가
    
    * design : 홈 배너 추가
    
    ---------
    
    Co-authored-by: kyumho kim <js030@github.com>

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,19 @@
+import HomeBanner from '@/components/HomeBanner'
 import Link from 'next/link'
 
 export default function page() {
   return (
     <section className='flex flex-col justify-center items-center max-w-[850px] mx-auto my-20 mt-10'>
-      <h1 className='flex items-center basis-1/12'>메인페이지</h1>
+      <div className='w-screen'>
+        <HomeBanner />
+      </div>
+
       <div className='max-w-[500px] w-full flex flex-col justify-center gap-10 mt-10 lg:flex-row lg:gap-24 basis-11/12 '>
         <Link href='/hotel/reserve' className='btn lg:w-32'>
           예약하기
         </Link>
         <Link href='/hotel/payment' className='btn lg:w-32 '>
           결제하기
-        </Link>
-        <Link href='/auth/signin' className='btn lg:w-32 '>
-          로그인
-        </Link>
-        <Link href='/auth/signup' className='btn lg:w-32 '>
-          회원가입
-        </Link>
-        <Link href='/mypage' className='btn lg:w-32 '>
-          마이페이지
-        </Link>
-        <Link href='/hotel/register' className='btn lg:w-32 '>
-          숙소 등록
         </Link>
       </div>
     </section>
