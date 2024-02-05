8d54b30d617adc8cb459ad54fa0e8f11dbccadec
jkeum <jon9eun@gmail.com>
2024-02-05 17:20:50 +0900

    예약하기 페이지 달력 수정, 결제 금액 수정 (#13)
    * fix : 예약 취소 버튼을 눌렀을 때 다른 페이지로 이동, 체크인 날짜부터는 예약 취소 버튼 비활성화
    
    결제 취소를 위해 데이터도 넘겨줘야 할 것 같음 - 다시 확인 필요
    예약 취소 완료 페이지로 이동해야 하는데 아직 url을 몰라서 나중에 수정해야 함
    
    * feat : 에약 취소내역 상세페이지 구현
    
    * fix : 예약 취소된 내역을 예약 상세페이지로 접근하면 예약 취소 상세페이지를 렌더링
    
    * refactor : 필요 없어진 중복 코드 제거
    
    * fix : 예약 상세페이지에 호스트 정보 추가
    
    * fix : 예약 상세페이지 url 변경
    
    /reserve/{reserveId} -> /reserve/detail/{reserveId}
    /reserve/{hotelId}가 필요해서 변경함
    
    * feat : 예약하기 페이지 화면 기본 구성
    
    * fix : 예약하기 페이지 달력 수정
    
    * fix : 예약하기 페이지에서 인원 선택 시 최대 인원 넘어가지 않도록 설정
    
    * fix : 예약하기 페이지 날짜 선택 화면 수정
    
    체크인 날짜, 체크아웃 날짜 각각 달력 표시
    현재 날짜부터 선택 가능, 현재 날짜부터 2년 뒤까지만 선택 가능
    react-datepicker, date-fns, dayjs, @mui/icons-material @mui/material @emotion/styled @emotion/react, lodash 설치 필요
    아직 예약 불가능한 날짜 비활성화 기능은 없음
    
    * fix : 예약하기 페이지 달력에서 선택 불가능한 날짜 비활성화 설정
    
    아직 DB에서 예약되어있는 날짜를 조회하진 않음
    샘플 코드로 선택 불가능한 날짜를 설정했을 때 제대로 동작하는지만 테스트
    
    * fix : 예약하기 페이지 결제 금액 정보 추가
    
    현재는 단순히 1박 가격만 띄워줌. 수정 필요
    
    * fix : 예약하기 페이지 호텔 정보 및 호스트 정보 수정
    
    * fix : HotelReservation에서 isLoading -> isHotelLoading 수정
    
    * feat : 이용 완료된 예약에 한하여 리뷰 작성 버튼 추가
    
    * fix : Calendar에서 사용하던 startDate, endDate와 useState를 HotelReservation으로 이동
    
    HotelReservation에서 그 값을 사용하기 위함
    Calendar에는 props로 전달함
    
    * feat : 예약하기 POST
    
    체크인/체크아웃 날짜, 선택한 기간에 대한 결제 요금 전송
    isPaid는 false로 전송해서 아직 결제가 되지 않았음을 나타냄(예약 완료되지 않음)
    
    * feat : 멤버 관련해서 접근 권한 설정
    
    * fix : 예약 기간 계산하는 코드 수정, 주석 제거
    
    * feat : 예약하기 버튼 누르면 결제하기 페이지로 라우팅
    
    * feat : 예약할 때 날짜 선택 시 해당 숙소에 예약되어 있는 날짜들 비활성화하기 위한 GET 요청
    
    * fix : 예약하기 페이지 캘린더 수정
    
    * fix : 선택한 체크인/체크아웃 날짜가 디비에는 하루씩 당겨져서 저장되는 문제 해결, 날짜 선택할 때마다 결제 금액 변경
    
    * refactor : 사용하지 않는 달력 코드 삭제

--- src/components/reservation/HotelReservation.js
+++ src/components/reservation/HotelReservation.js
@@ -1,22 +1,34 @@
 'use client'
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHotelDetail } from '@/hooks/useHotel';
-import { Button, Spacer, Input, Select } from '@nextui-org/react';
+import { Button, Spacer, Input } from '@nextui-org/react';
 import { useRouter } from 'next/navigation';
 import 'react-datepicker/dist/react-datepicker.css';
-import CalendarCustom from './Calendar';
+// import CalendarCustom from './Calendar';
+import CalendarCustom from './CalendarCustom';
 import axios from '@/config/axios-config'
+import { differenceInCalendarDays, addDays } from 'date-fns';
 
 export default function HotelReservation({ id }) {
 	const { hotel, isHotelLoading, isError, error } = useHotelDetail(id);
 	const [guestCount, setGuestCount] = useState(1);
 
 	// CalendarCustom에 props로 전달
-	const [startDate, setStartDate] = useState(new Date());
-	const [endDate, setEndDate] = useState(new Date());
+	const [startDate, setStartDate] = useState(new Date(new Date().setHours(0, 0, 0, 0)));
+	const [endDate, setEndDate] = useState(addDays(new Date(new Date().setHours(0, 0, 0, 0)), 1));
+	const [totalPrice, setTotalPrice] = useState(0); // 가격 상태 추가
 
 	const router = useRouter();
+	
+	useEffect(() => {
+		// 호텔 정보가 로딩된 후에 가격 계산
+		if (!isHotelLoading && hotel) {
+				const daysDiff = differenceInCalendarDays(endDate, startDate) || 1; // 최소 1일은 계산
+				const calculatedPrice = daysDiff * hotel.price;
+				setTotalPrice(calculatedPrice);
+		}
+	}, [startDate, endDate, hotel, isHotelLoading]);
 
 	if (isHotelLoading) {
 		return <div>loading</div>
@@ -45,27 +57,26 @@ export default function HotelReservation({ id }) {
 		});
 	};
 
-
 	// Submit the reservation
 	const handleSubmit = async () => {
-		// 시작 날짜와 종료 날짜의 시간을 자정으로 설정
-		const start = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate());
-		const end = new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate());
-
 		// 날짜 차이 계산
-		const timeDiff = end - start;
-		const daysDiff = timeDiff / (1000 * 3600 * 24);
+		const daysDiff = differenceInCalendarDays(endDate, startDate);
 
 		// 호텔 가격 계산
 		const calculatedPrice = daysDiff * hotel.price;
 
+		// 시작 날짜와 종료 날짜 설정
+		const formattedStartDate = new Date(startDate.getTime() + (24 * 60 * 60 * 1000)).toISOString().substring(0, 10);
+		const formattedEndDate = new Date(endDate.getTime() + (24 * 60 * 60 * 1000)).toISOString().substring(0, 10);
+
 		const reservationInfo = {
 			numOfGuests: guestCount,
-			checkInDate: startDate,
-			checkOutDate: endDate,
+			checkInDate: formattedStartDate,
+			checkOutDate: formattedEndDate,
 			price: calculatedPrice,
 			isPaid: false
 		};
+		console.log(reservationInfo);
 	
 		try {
 			const response = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/reserve/${id}`, reservationInfo, {
@@ -80,7 +91,6 @@ export default function HotelReservation({ id }) {
 
 			// Redirect or show success message
 			const reserveId = response.data.objData.id;
-			console.log('reserveId: ', reserveId);
 			router.push(`/cashLog/payByCash/${reserveId}`);
 		} catch (error) {
 			console.error('Error making reservation:', error);
@@ -95,6 +105,7 @@ export default function HotelReservation({ id }) {
 					
 					<label htmlFor="date">날짜 선택</label>
 					<CalendarCustom
+						hotelId={id}
 						startDate={startDate}
 						endDate={endDate}
 						setStartDate={setStartDate}
@@ -116,8 +127,10 @@ export default function HotelReservation({ id }) {
 					</div>
 					<Spacer y={1} />
 
-					<h3>환불 정책</h3>
-					<p>...환불 정책 내용...</p>
+					<div style={styles.refundPolicy}>
+						<h3>환불 정책</h3>
+						<p>...환불 정책 내용...</p>
+					</div>
 				</div>
 
 				<div style={styles.divider}></div>
@@ -134,7 +147,7 @@ export default function HotelReservation({ id }) {
 
 					<div style={styles.paymentAmount}>
 						<h3 style={styles.paymentAmountTitle}>결제 금액</h3>
-						<p style={styles.paymentAmountValue}>{hotel.price}원</p>
+						<p style={styles.paymentAmountValue}>{totalPrice}원</p>
 					</div>
 					
 					<div style={styles.paymentDetails}>
@@ -175,6 +188,7 @@ const styles = {
 		display: 'flex',
 		flexDirection: 'column',
 		alignSelf: 'flex-start',
+		justifyContent: 'space-between',
 	},
 	title: {
 		fontSize: '1.5rem',
@@ -182,18 +196,11 @@ const styles = {
 		marginBottom: '20px',
 		flex: 1,
 	},
-	dateInput: {
-		width: '100%',
-		padding: '10px',
-		marginBottom: '10px',
-		border: '1px solid #ddd',
-		borderRadius: '4px',
-	},
 	guestCountContainer: {
 		display: 'flex',
 		flexDirection: 'column',
 		alignItems: 'flex-start',
-		margin: '80px 0',
+		margin: '20px 0',
 	},
 	guestCountLabel: {
 		marginBottom: '10px',
@@ -207,6 +214,9 @@ const styles = {
 	},
 	guestCountNumber: {
 		textAlign: 'center',
+	},
+	refundPolicy: {
+		margin: '20px 0',
 	},
 	hotelInfo: {
 		display: 'flex',
@@ -272,7 +282,6 @@ const styles = {
 		alignSelf: 'flex-start',
 	},
 	button: {
-		padding: '10px 20px',
 		backgroundColor: '#EF4444',
 		color: 'white',
 		borderRadius: '5px',
--- src/hooks/useReservation.js
+++ src/hooks/useReservation.js
@@ -3,28 +3,53 @@ import axios from "@/config/axios-config";
 
 /**  상세 정보 */
 const fetchReservationDetail = async (reserveId) => {
-  const res = await axios.get(
+	const res = await axios.get(
 		`api/v1/reserve/detail/${reserveId}`, {
 			...axios.defaults,
 			useAuth: true
 	});
-  console.log(res);
-  return res.data;
+	console.log(res);
+	return res.data;
 };
 
 export const useReservationDetail = (reserveId) => {
-  const {
-    data: reservation,
-    isLoading,
-    isFetching,
-    isError,
-    error,
-  } = useQuery({
-    queryKey: ["reservationDetail", reserveId],
-    queryFn: () => fetchReservationDetail(reserveId),
-  });
+	const {
+		data: reservation,
+		isLoading,
+		isFetching,
+		isError,
+		error,
+	} = useQuery({
+		queryKey: ["reservationDetail", reserveId],
+		queryFn: () => fetchReservationDetail(reserveId),
+	});
 
-  console.log("reserverId = " + reserveId);
+	console.log("reserverId = " + reserveId);
 
-  return { reservation, isLoading, isFetching, isError, error };
+	return { reservation, isLoading, isFetching, isError, error };
+};
+
+/** 숙소의 예약되어 있는 날짜 정보 */
+const fetchReservedDatesOfHotel = async (hotelId) => {
+	const res = await axios.get(
+		`api/v1/reserve/reservedDates/${hotelId}`);
+	return res.data.objData.reservedDates;
+};
+
+
+export const useReservedDatesOfHotel = (hotelId) => {
+	const {
+		data: reservedDates,
+		isLoading,
+		isFetching,
+		isError,
+		error,
+	} = useQuery({
+		queryKey: ["reservedDates", hotelId],
+		queryFn: () => fetchReservedDatesOfHotel(hotelId),
+	});
+
+	console.log("hotelId = " + hotelId);
+
+	return { reservedDates, isLoading, isFetching, isError, error };
 };
