c7a195fc063cd9d683316a049977f08b5cd07abf
js030 <rlaruagh030@gmail.com>
2024-02-14 08:12:23 +0000

    feat: git pull

--- src/components/hotel/search/SearchCity.js
+++ src/components/hotel/search/SearchCity.js
@@ -27,15 +27,15 @@ export default function SearchCity({ city, setCity }) {
           서울
         </AutocompleteItem>
         <AutocompleteItem
-          key='경기도'
+          key='경기'
           startContent={
             <Avatar
-              alt='경기도'
+              alt='경기'
               className='w-6 h-6'
               src='https://i.namu.wiki/i/RhnVU3OJCrIrRfLuThp7ZTixaYMSxqG1QkzHPLLkHdavjg6NtRpNYRNt9e_PWdsvOINWa9QiDl28GPu64bzpVA.svg'
             />
           }>
-          경기도
+          경기
         </AutocompleteItem>
         <AutocompleteItem
           key=''
@@ -104,81 +104,81 @@ export default function SearchCity({ city, setCity }) {
           광주
         </AutocompleteItem>
         <AutocompleteItem
-          key='강원도'
+          key='강원'
           startContent={
             <Avatar
-              alt='강원도'
+              alt='강원'
               className='w-6 h-6'
               src='https://i.namu.wiki/i/nPVK6K7WdPBss5rV-K18AEuV8Zy6HXoeXqUVAEimHVg0-mzq6X0A3B4kTsMjoXqVPwx4H9CKXr66Tc8lomifAw.svg'
             />
           }>
-          강원도
+          강원
         </AutocompleteItem>
         <AutocompleteItem
-          key='충청북도'
+          key='충북'
           startContent={
             <Avatar
-              alt='충청북도'
+              alt='충북'
               className='w-6 h-6'
               src='https://blog.kakaocdn.net/dn/dtSEb0/btrFqbm5a72/jwvxvA218zA8vm0LTkc1KK/img.png'
             />
           }>
-          충청북도
+          충북
         </AutocompleteItem>
         <AutocompleteItem
-          key='충청남도'
+          key='충남'
           startContent={
             <Avatar
-              alt='충청남도'
+              alt='충남'
               className='w-6 h-6'
               src='https://i.namu.wiki/i/NmdGLbvm2p5mv5KvKEA_R-PUD4Ts5q5KLVdjqZVaYDU7_yFt0ihtT_5dVIPAXthNxcynMAGiMpF30iJldEoj7w.svg'
             />
           }>
-          충청남도
+          충남
         </AutocompleteItem>
         <AutocompleteItem
-          key='전라북도'
+          key='전북'
           startContent={
             <Avatar
-              alt='전라북도'
+              alt='전북'
               className='w-6 h-6'
               src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCF_eBcqcjQWwWTlXTwbJl1tMPJf0plLHSOg&usqp=CAU'
             />
           }>
-          전라북도
+          전북
         </AutocompleteItem>
         <AutocompleteItem
-          key='전라남도'
+          key='전남'
           startContent={
             <Avatar
-              alt='전라남도'
+              alt='전남'
               className='w-6 h-6'
               src='https://i.namu.wiki/i/D4yPLU922vUfJN21JbmLvYlEZQk3as7MFiPdpL_1qYe1E1YjJfcGM5mVTnAtMl2uYNYJ8BRz3pZHXiEE7kYCzA.svg'
             />
           }>
-          전라남도
+          전남
         </AutocompleteItem>
         <AutocompleteItem
-          key='경상북도'
+          key='경북'
           startContent={
             <Avatar
-              alt='경상북도'
+              alt='경북'
               className='w-6 h-6'
               src='https://www.gb.go.kr/Main/Images/new/ko/contents/symbol01.png'
             />
           }>
-          경상북도
+          경북
         </AutocompleteItem>
         <AutocompleteItem
-          key='경상남도'
+          key='경남'
           startContent={
             <Avatar
-              alt='경상남도'
+              alt='경남'
               className='w-6 h-6'
               src='https://i.namu.wiki/i/vg6QNmQ5SqCZluJkG0jiTibKqlrbGMVdijw-ts80zx90o3HLiTiXTTqCTB4CJUyvtGI343zzK3cU6eMrUyBOMg.svg'
             />
           }>
-          경상남도
+          경남
         </AutocompleteItem>
       </Autocomplete>
     </div>
